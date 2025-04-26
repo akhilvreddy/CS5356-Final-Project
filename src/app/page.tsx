ab800894075be51bd0e08b8f808e83b8216f9f6f
Akhil Reddy <akhilvreddy@dhcp-vl2053-73.cornell-visitor.cit.cornell.edu>
2025-04-26 12:42:31 -0400

    pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,8 +104,8 @@ export default function Home() {
           >
             <a 
               href="https://github.com/akhilvreddy/CS5356-Final-Project" 
-              target="_blank" 
-              rel="noopener noreferrer"
+          target="_blank"
+          rel="noopener noreferrer"
               className="flex items-center space-x-2 bg-gray-900/30 backdrop-blur-sm px-3 py-2 rounded-full hover:bg-gray-800/40 transition-colors duration-300"
             >
               <svg 
@@ -119,7 +119,7 @@ export default function Home() {
                 strokeLinecap="round" 
                 strokeLinejoin="round" 
                 className="text-gray-300"
-              >
+        >
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
               </svg>
               <span className="text-xs text-gray-300">GitHub</span>
@@ -166,7 +166,7 @@ export default function Home() {
             ))}
           </div>
         )}
-      </div>
+    </div>
     </>
   );
 }
